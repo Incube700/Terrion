680c953aef7953bfb071d06cb1d6c3564a98bf22
Incube700 <belkajor@gmail.com>
2025-06-30 04:33:52 +0530

    feat: clean minimal project state, all scenes and scripts recreated via Godot editor, ready for further development per new README plan

--- scripts/EnemyAI.gd
+++ scripts/EnemyAI.gd
@@ -5,25 +5,25 @@ var ai_timer: Timer
 var decision_timer: Timer
 
 func _init(_battle_manager):
-    battle_manager = _battle_manager
-    ai_timer = Timer.new()
-    ai_timer.wait_time = 5.0
-    ai_timer.autostart = true
-    ai_timer.timeout.connect(_on_ai_spawn)
-    add_child(ai_timer)
+	battle_manager = _battle_manager
+	ai_timer = Timer.new()
+	ai_timer.wait_time = 5.0
+	ai_timer.autostart = true
+	ai_timer.timeout.connect(_on_ai_spawn)
+	add_child(ai_timer)
 
-    decision_timer = Timer.new()
-    decision_timer.wait_time = 3.0
-    decision_timer.autostart = true
-    decision_timer.timeout.connect(_on_ai_decision)
-    add_child(decision_timer)
+	decision_timer = Timer.new()
+	decision_timer.wait_time = 3.0
+	decision_timer.autostart = true
+	decision_timer.timeout.connect(_on_ai_decision)
+	add_child(decision_timer)
 
 func _on_ai_spawn():
-    if battle_manager and battle_manager.enemy_energy >= 20:
-        battle_manager.spawn_unit_at_pos("enemy", Vector3(4, 0, 12), "soldier")
-        battle_manager.enemy_energy -= 20
-        battle_manager.update_hud()
+	if battle_manager and battle_manager.enemy_energy >= 20:
+		battle_manager.spawn_unit_at_pos("enemy", Vector3(4, 0, 12), "soldier")
+		battle_manager.enemy_energy -= 20
+		battle_manager.update_hud()
 
 func _on_ai_decision():
-    # Здесь можно добавить более сложную логику принятия решений
-    pass 
+	# Здесь можно добавить более сложную логику принятия решений
+	pass 
